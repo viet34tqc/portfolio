f5f3b95692575514f2f023f4756d2c48fbaa7e84
viet34tqc <viet34tqc@gmail.com>
2021-06-01 00:20:44 +0700

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 const Header = () => {
 	return (
-		<div>
-			header
-		</div>
-	)
-}
+		<header className="d-flex space-between align-center">
+			<Link href="/">
+				<a className="header-logo">VietNguyen</a>
+			</Link>
 
-export default Header
+			<nav class="header-nav d-flex space-between">
+				<Link href="/">Blog</Link>
+				<Link href="mail:viet34tqc@gmail.com">Contact</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
 
