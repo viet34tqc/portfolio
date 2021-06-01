f0cdac12866c9de90252982e766d294e1d0ebe07
viet34tqc <viet34tqc@gmail.com>
2021-06-01 07:54:15 +0700

    Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
 const Intro = () => {
-    return (
-        <div>
-            Intro
-        </div>
-    );
+	return (
+		<section className="intro">
+			<div className="container">
+				<div className="intro__left">
+					<p className="intro__greeting">Hi, I'm VietNguyen</p>
+					<p>a web developer and freelancer</p>
+				</div>
+				<div className="intro__right">
+					<p>I'm a developer based in Ha Noi, Viet Nam. I have started my Web development since 2013 and focus mostly on <b>WordPress</b>, <b>ReactJS</b> and other <b>Front-end</b> technologies.</p>
+					<p>In my free time, I do freelancing to help other people build their own website.</p>
+					<p>You can contact me by email at <strong>viet34tqc@gmail.com</strong></p>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Intro;
