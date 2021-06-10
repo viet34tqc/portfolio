53625c877dd9583b17a6b46a5e305ae27f1d58b2
viet34tqc <viet34tqc@gmail.com>
2021-06-10 12:21:59 +0700

    Update image size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,29 @@
 import Link from "next/link";
+import {useState, useEffect} from 'react';
 
 const Header = () => {
+	const [theme, setTheme] = useState( 'light' );
+
+	useEffect( () => {
+		document.documentElement.setAttribute(
+			"data-theme",
+			localStorage.getItem( 'theme' )
+		);
+	}, [] )
+
+	const toggleDarkMode = function () {
+		saveTheme( theme === 'dark' ? 'light' : 'dark' );
+	}
+
+	const saveTheme = function ( theme ) {
+		setTheme( theme );
+		localStorage.setItem( 'theme', theme );
+		document.documentElement.setAttribute(
+			"data-theme",
+			theme
+		);
+	}
+
 	return (
 		<header className="d-flex space-between align-center">
 			<Link href="/">
@@ -10,6 +33,9 @@ const Header = () => {
 			<nav className="header-nav d-flex space-between">
 				<Link href="/">Blog</Link>
 				<Link href="mail:viet34tqc@gmail.com">Contact</Link>
+				<button className="header-darkmode" onClick={toggleDarkMode}>
+					<svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="currentColor"><path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path></svg>
+				</button>
 			</nav>
 		</header>
 	);
